e69261f306116116c54e7f0613d1137292d94240
Paul Young <yyang173@illinois.edu>
2019-02-07 10:47:00 -0600

    compiled but segfaults with intel/17.0.3

--- build.sh
+++ build.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# define quantum espresso location
+# !!!! pw must be compiled with -fPIC
+export QE_HOME=/home/yyang173/soft/espresso-5.1.0
+
+# define interface location
 export PWINTERFACE_HOME=./src/Interfaces/PWSCF
 
+cwd=`pwd`
+
+# build interface first
+cd $PWINTERFACE_HOME
+make
+cd $cwd
+
+# build qmcpack
 mkdir build
 cd build
-
 cmake -DCMAKE_C_COMPILER=mpiicc -DCMAKE_CXX_COMPILER=mpiicpc ..
 make -j 24
